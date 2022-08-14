d5c1ec419766c911b5ebd47451570a280ac4f98f
Fernando Lopez <flopez00032@gmail.com>
2022-08-14 18:35:09 -0500

    minute 7 of video

---
+++ Random-Website.js
@@ -0,0 +1,22 @@
+
+// INTRODUCTION
+// let yourName = prompt ("Enter your name here please");
+// if (yourName != null){
+//     document.getElementById("sayHello").innerHTML = "Hello " + yourName;
+// } else {
+//     alert("You just couldn\'t follow instructions huh?");
+// }
+
+// MATH
+
+document.write("2 + 3 = ", 2 + 3, "<br/>");
+document.write("2 - 3 = ", 2 - 3, "<br/>");
+document.write("2 * 3 = ", 2 * 3, "<br/>");
+document.write("4 / 3 = ", 4 / 3, "<br/>");
+document.write("2 % 3 = ", 2 % 3, "<br/>");
+
+document.write("Max Num = ", Number.MAX_VALUE, "<br/>");
+document.write("Min Num = ", Number.MIN_VALUE,"<br/>");
+
+precisionTest = 0.1000000000000001;
+document.write(precisionTest + 0.1000000000000001, "<br/>");
