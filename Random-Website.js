c1e071c589761db1776e90dced15345ef747feb2
Fernando Lopez <flopez00032@gmail.com>
2022-08-21 10:40:21 -0500

    randome number

--- Random-Website.js
+++ Random-Website.js
@@ -4,7 +4,7 @@
 // if (yourName != null){
 //     document.getElementById("sayHello").innerHTML = "Hello " + yourName;
 // } else {
-//     alert("You just couldn\'t follow instructions huh?");
+//     alert("You just couldn't follow instructions huh?");
 // }
 
 // MATH
@@ -21,3 +21,11 @@ document.write("Min Num = ", Number.MIN_VALUE,"<br/>");
 // will not be precise after 16 digits
 precisionTest = 0.1000000000000001;
 document.write(precisionTest + 0.1000000000000001, "<br/>");
+// rounding
+let balance = 1563.87;
+
+// random number increment
+let ranNumb = 5;
+
+document.write("Monthly payment : ", (balance / 12).toFixed(2),"<br />");
+document.write("randNumb++ = ", ranNumb)
