d9721be33bd1d13c030745f42f0025e1a5241755
Fernando Lopez <flopez00032@gmail.com>
2022-09-04 10:41:22 -0500

    return string with mesage

--- js/js-functions-practice.js
+++ js/js-functions-practice.js
@@ -1,3 +1,8 @@
-function reverse(input){
-    return input.toString();
+// practice function that returns an input //
+function echo(input){
+    return input;
+}
+//ternary form with condition to for string input //
+function logInput(input){
+    console.log(( typeof input ==='string')? input + " is a string" : input + " is not a string");
 }
